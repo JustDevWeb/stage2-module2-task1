9b04159b4270cc94ca69dbfb0626afbbb2ce5545
JustDevWeb <zm91672@gmail.com>
2023-10-04 11:19:18 +0500

    stage-2-module-2-servlets done

--- src/main/java/com/example/servlet/AddUserServlet.java
+++ src/main/java/com/example/servlet/AddUserServlet.java
@@ -1,5 +1,43 @@
 package com.example.servlet;
 
-public class AddUserServlet {
-    //write your code here!
+import com.example.User;
+import com.example.Warehouse;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/add")
+public class AddUserServlet extends HttpServlet {
+
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/html");
+
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/jsp/add.jsp");
+        dispatcher.include(req , resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+       String firstName = req.getParameter("firstName");
+       String lastName = req.getParameter("lastName");
+
+       if( ( firstName != null && lastName != null ) && ( !firstName.isEmpty() && !lastName.isEmpty() ) ) {
+           User user = new User( firstName , lastName );
+           Warehouse warehouse = Warehouse.getInstance();
+           warehouse.addUser( user );
+
+           req.setAttribute("user" , user);
+
+           RequestDispatcher dispatcher = req.getRequestDispatcher("/jsp/add.jsp");
+           dispatcher.include(req , resp);
+        }
+    }
 }
